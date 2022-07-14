bfb376e9d2618586701c408f055a4ae9705198f6
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2022-07-13 23:24:10 -0300

    simple flow - everything on main

--- src/app.js
+++ src/app.js
@@ -1,4 +1,23 @@
 import { createGitgraph } from "@gitgraph/js";
 
-const graphContainer = document.getElementById("gitgraph");
+const graphContainer = document.getElementById("gitgraph_option1");
 const gitgraph = createGitgraph(graphContainer);
+
+const main = gitgraph.branch("main");
+main
+  .commit({
+    author: "User 1 <user1@provider.com>",
+    subject: "Init the project",
+  })
+  .commit({
+    author: "User 2 <user2@provider.com>",
+    subject: "Add README",
+  })
+  .commit({
+    author: "User 2 <user2@provider.com>",
+    subject: "Add tests",
+  })
+  .commit({
+    author: "User 1 <user1@provider.com>",
+    subject: "Implement feature",
+  });
